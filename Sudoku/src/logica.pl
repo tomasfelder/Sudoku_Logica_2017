0b83cbe857b3962ddb2b66b03c1caf070b3f9525
tomasfelder <tomas.felder@gmail.com>
2017-05-01 11:32:53 -0300

    Comprobar 2

--- Sudoku/src/logica.pl
+++ Sudoku/src/logica.pl
@@ -38,7 +38,7 @@ nroValido(6).
 nroValido(7).
 nroValido(8).
 nroValido(9).
-
+coord(X,Y):-nroValido(X),nroValido(Y).
 listaNumeros([1,2,3,4,5,6,7,8,9]).
 
 coordenadas( [[1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7], [1, 8], [1, 9], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [3, 9], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9], [5, 1], [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7], [5, 8], [5, 9], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7], [6, 8], [6, 9], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7], [7, 8], [7, 9], [8, 1], [8, 2], [8, 3], [8, 4], [8, 5], [8, 6], [8, 7], [8, 8], [8, 9], [9, 1], [9, 2], [9, 3], [9, 4], [9, 5], [9, 6], [9, 7], [9, 8], [9, 9]]).
@@ -66,13 +66,17 @@ reemplazar(Nuevo,N,[X|L],[X|LNueva]):- Aux is N-1, reemplazar(Nuevo,Aux,L,LNueva
 comprobar(TableroF):-coordenadas(C),comprobar_aux(TableroF,C).
 
 comprobar_aux(_,[]).
-comprobar_aux(TableroF,[[X,Y]|C]):-esCero(TableroF,X,Y),listaNumeros(L),
-									asignarNumero(TableroF,X,Y,L,TableroNuevo),comprobar_aux(TableroNuevo,C).
+comprobar_aux(TableroF,[[X,Y]|C]):-esCero(TableroF,X,Y),nroValido(N),
+    				agregar(N,X,Y,TableroF,TableroNuevo)
+    						,comprobar_aux(TableroNuevo,C).
 comprobar_aux(TableroF,[[_,_]|C]):-comprobar_aux(TableroF,C).
+
+comprobar2(TableroF):-not(fin_sudoku(TableroF)),coord(X,Y),
+                          esCero(TableroF,X,Y),
+    		nroValido(N),agregar(N,X,Y,TableroF,TableroNuevo),
+    				comprobar2(TableroNuevo).
 
 esCero(TableroF,X,Y):-obtenerLista(X,TableroF,L),obtenerLista(Y,L,E),E=0.
 
-asignarNumero(TableroF,X,Y,[],TableroF).
-asignarNumero(TableroF,X,Y,[E|L],TableroNuevo):-agregar(E,X,Y,TableroF,TableroNuevo),
-											asignarNumero(TableroF,X,Y,L,TableroNuevo).
-asignarNumero(TableroF,X,Y,[_|L],TableroNuevo):-asignarNumero(TableroF,X,Y,L,TableroNuevo).
+fin_sudoku([]).
+fin_sudoku(Fila|Resto):-no_pertenece(0,Fila),fin_sudoku(Resto).
